d4da275c8da80bde06a2426c743bac4eaaf400f0
Abhishek <hayagreevvishnu@gmail.com>
2026-01-08 23:03:43 +0530

    Fixed the errors

--- connectionengine.client/src/app/app.routes.ts
+++ connectionengine.client/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { SignupComponent } from './auth/signup/signup.component';
 import { LoginComponent } from './auth/login/login.component';
+import { ProfileComponent } from './User/profile/profile.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'signup', pathMatch: 'full' },
   { path: 'signup', component: SignupComponent },
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'profile', component:ProfileComponent }
 ];
